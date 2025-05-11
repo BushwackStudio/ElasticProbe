3cd63882c9276a10ee3760b5b642a1687c123c3f
Nima Shayanfar <nshayanfar@gmail.com>
2025-05-11 12:53:31 +0330

    fix: update solution messaging for field limit error in Elasticsearch tests

--- tests/php/TestElasticsearchErrorInterpreter.php
+++ tests/php/TestElasticsearchErrorInterpreter.php
@@ -127,9 +127,9 @@ class TestElasticsearchErrorInterpreter extends BaseTestCase {
 
 		$error = 'Limit of total fields [1000] in index [elasticpresstest-post-1] has been exceeded';
 		if ( Utils\is_epio() ) {
-			$solution = 'Your website content has more public custom fields than WPProbe.com is able to store. Check our articles about <a href="https://www.elasticpress.io/documentation/article/i-get-the-error-limit-of-total-fields-in-index-has-been-exceeded/">Elasticsearch field limitations</a> and <a href="https://www.elasticpress.io/documentation/article/how-to-exclude-metadata-from-indexing/">how to index just the custom fields you need</a> and run sync again.';
+			$solution = 'Your website content has more public custom fields than WPProbe.com is able to store. Check our articles about <a href="https://www.elasticpress.io/documentation/article/i-get-the-error-limit-of-total-fields-in-index-has-been-exceeded/">Elasticsearch field limitations</a> and <a href="https://www.elasticpress.io/documentation/article/how-to-exclude-metadata-from-indexing/">how to index just the custom fields you need</a> and sync again.';
 		} else {
-			$solution = 'Your website content has more public custom fields than Elasticsearch is able to store. Check our articles about <a href="https://www.elasticpress.io/documentation/article/i-get-the-error-limit-of-total-fields-in-index-has-been-exceeded/">Elasticsearch field limitations</a> and <a href="https://www.elasticpress.io/documentation/article/how-to-exclude-metadata-from-indexing/">how to index just the custom fields you need</a> and run sync again.';
+			$solution = 'Your website content has more public custom fields than Elasticsearch is able to store. Check our articles about <a href="https://www.elasticpress.io/documentation/article/i-get-the-error-limit-of-total-fields-in-index-has-been-exceeded/">Elasticsearch field limitations</a> and <a href="https://www.elasticpress.io/documentation/article/how-to-exclude-metadata-from-indexing/">how to index just the custom fields you need</a> and sync again.';
 		}
 
 		$suggested = $error_interpreter->maybe_suggest_solution_for_es( $error );
