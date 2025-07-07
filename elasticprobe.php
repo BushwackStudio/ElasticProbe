4d4873364b74f43559b356aece48f4a752565c6e
Nima Shayanfar <nshayanfar@gmail.com>
2025-07-07 12:26:40 +0330

    [Changed] Bumped version to 1.1.0 [Fixed] PHP linting warning [Added] PatchStack link to readme.txt

--- elasticprobe.php
+++ elasticprobe.php
@@ -3,7 +3,7 @@
  * Plugin Name:       ElasticProbe
  * Plugin URI:        https://github.com/BushwackStudio/ElasticProbe
  * Description:       Supercharge your WordPress search with ElasticSearch® precision.
- * Version:           1.0.0
+ * Version:           1.1.0
  * Requires at least: 6.2
  * Requires PHP:      7.4
  * Author:            BushwackStudio
@@ -34,7 +34,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'EPROBE_URL', plugin_dir_url( __FILE__ ) );
 define( 'EPROBE_PATH', plugin_dir_path( __FILE__ ) );
 define( 'EPROBE_FILE', plugin_basename( __FILE__ ) );
-define( 'EPROBE_VERSION', '1.0.0' );
+define( 'EPROBE_VERSION', '1.1.0' );
 
 define( 'EPROBE_PHP_VERSION_MIN', '7.4' );
 
--- tests/php/features/TestInstantResults.php
+++ tests/php/features/TestInstantResults.php
@@ -19,7 +19,6 @@ class TestInstantResults extends BaseTestCase {
 	 * @group instant-results
 	 */
 	public function test_get_settings_schema() {
-		// $this->markTestSkipped( 'Requires instant results' );
 
 		$settings_schema = \ElasticProbe\Features::factory()->get_registered_feature( 'instant-results' )->get_settings_schema();
 
